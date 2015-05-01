64872bd507aa57932010dbc53f4a282c518bf9d4
Stefan-Code <Stefan.github@gmail.com>
2015-05-01 15:49:31 +0800

    added bash-bang to nose.sh

--- nose.sh
+++ nose.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 nosetests -v --with-coverage --cover-inclusive --cover-erase --cover-html --cover-html-dir=./coverage --cover-package=gglsbl3 --with-html-report --html-output-file ./coverage/report.html
 chromium-browser  --new-window
 chromium-browser ./coverage/index.html
