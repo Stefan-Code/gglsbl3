718e68629be2bcee7d00028b119bd14964b54751
Stefan Kuntz <Stefan.github@gmail.com>
2015-11-10 22:17:25 +0100

    bump version, update email and classifiers

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3.4
+#!/usr/bin/env python3
 
 from setuptools import setup
 
@@ -6,9 +6,9 @@ import sys
 import os
 
 if not sys.version[0]  == "3":
-    raise Exception("This Program is for Python VERSION 3 only!")
+    raise Exception("This Program is for Python **VERSION 3** only!")
 
-__version__ = '0.0.1'
+__version__ = '0.1.0'
 here = os.path.abspath(os.path.dirname(__file__))
 
 # Get the long description from the relevant file
@@ -19,14 +19,19 @@ setup(name='gglsbl3',
       version=__version__,
       description="Client library for Google Safe Browsing API",
       classifiers=[
-          "Programming Language :: Python :: 3",
-          "Topic :: Internet",
-          "Topic :: Software Development :: Libraries :: Python Modules",
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Intended Audience :: Developers',
+          'Topic :: Internet',
+          'Topic :: Software Development :: Libraries :: Python Modules',
       ],
       long_description=long_description,
       keywords='gglsbl3 gglsbl safebrowsing google-safe-browing googlesafebrowsing',
-      author='Stefan-Code',
-      author_email='Stefan-Code@users.noreply.github.com',
+      author='Stefan Kuntz',
+      author_email='Stefan.github@gmail.com',
       url='https://github.com/Stefan-Code/gglsbl3',
       license='Apache2',
       packages=['gglsbl3', 'gglsbl3.logger', 'gglsbl3.settings', 'gglsbl3.util'],
