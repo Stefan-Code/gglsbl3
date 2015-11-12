20ec5db07c172aac3cfe035f2a2e55d916276379
Stefan Kuntz <Stefan.github@gmail.com>
2015-11-12 01:08:53 +0100

    more coverage in storage (storing of chunk)

--- tests/storage_test.py
+++ tests/storage_test.py
@@ -5,7 +5,10 @@ Created on Mar 2, 2015
 '''
 import unittest
 import os
+import pickle
+
 from gglsbl3 import storage
+from nose.tools import *
 
 
 class StorageTest(unittest.TestCase):
@@ -24,9 +27,24 @@ class StorageTest(unittest.TestCase):
         except Exception:
             raise
 
-    def testName(self):
-        pass
+    def test_expand_ranges(self):
+        compressed_ranges_list = ['1-4,7', '9-11', '50']
+        expanded_ranges = [1,2,3,4,7,9,10,11,50]
+        eq_(storage.StorageBase.expand_ranges(compressed_ranges_list), expanded_ranges)
 
+    def test_compress_ranges(self):
+        compressed_ranges = '1-4,7'
+        expanded_ranges = [1,2,3,4,7]
+        eq_(storage.StorageBase.compress_ranges(expanded_ranges), compressed_ranges)
+
+    @raises(ValueError)
+    def test_expand_ranges_fail(self):
+        _result = storage.StorageBase.expand_ranges(['1--7,-'])
+
+    def test_store_chunk(self):
+        with open('tests/fake_chunk.pickle', 'rb') as fake_chunk_file:
+            fake_chunk = pickle.load(fake_chunk_file)
+        self.storage.store_chunk(fake_chunk)
 
 if __name__ == "__main__":
     # import sys;sys.argv = ['', 'Test.testName']
