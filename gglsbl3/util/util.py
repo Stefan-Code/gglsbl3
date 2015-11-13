fcb7368ae97701fd9dbb60cb503f27bb8f204644
Stefan Kuntz <Stefan.github@gmail.com>
2015-11-13 17:17:39 +0100

    added docstrings

--- gglsbl3/util/util.py
+++ gglsbl3/util/util.py
@@ -5,13 +5,21 @@ Created on May 7, 2015
 '''
 
 def prettify_seconds(seconds):
+    """
+    Prettifies seconds.
+    Takes number of seconds (int) as input and returns a prettified string.
+
+    Example:
+    >>> prettify_seconds(342543)
+    '3 days, 23 hours, 9 minutes and 3 seconds'
+    """
     if seconds < 0:
         raise Exception("negative input not allowed")
     signs = {"s": {"singular": "second", "plural": "seconds", },
              "h": {"singular": "hour", "plural": "hours"},
              "min": {"singular": "minute", "plural": "minutes"},
              "d": {"singular": "day", "plural": "days"}
-             }
+            }
     seperator = ", "
     last_seperator = " and "
 
@@ -43,10 +51,14 @@ def format_max_len(string_to_format, max_len=15, replace="[...]"):
     """
     Formats a string so len(format_max_length(string_to_format)) <= max_len
     If the string_to_format is longer than max_len, it replaces characters in the middle with [...]
+
+    Example:
+    >>> util.format_max_len('abcdefghijklmnopqrstuvwxyz')
+    'abcde[...]vwxyz'
     """
-    real_max_len = max_len-len(replace)
+    real_max_len = max_len-len(replace)  # needed to count the [...] in the ouput length
     if len(string_to_format) <= max_len:
         return string_to_format
-    first = real_max_len//2
-    last = real_max_len-first
-    return string_to_format[0:first] + replace + string_to_format[len(string_to_format)-last:]
+    first = real_max_len // 2  # Insert the [...] in the (floored) middle
+    last = real_max_len - first
+    return string_to_format[0:first] + replace + string_to_format[len(string_to_format) - last:]
