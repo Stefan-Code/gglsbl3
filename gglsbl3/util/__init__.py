365b987334da898f2261d766facf4e79ac39ee27
Stefan Kuntz <Stefan.github@gmail.com>
2015-11-17 23:51:36 +0100

    added docstring

--- gglsbl3/util/__init__.py
+++ gglsbl3/util/__init__.py
@@ -1,2 +1,7 @@
+"""
+This util package provides utility functions
+that are used across the different gglsbl3 modules
+e.g. string formatting
+"""
 from .format_utils import prettify_seconds, format_max_len
 from .network_utils import int_to_ip, ip_to_int
