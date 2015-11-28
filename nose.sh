991cbea1a088822b0963f9a078f49bae0bdebca8
Stefan Kuntz <Stefan.github@gmail.com>
2015-11-28 10:15:18 +0100

    remove --cover-inclusive due to issues with the click library

--- nose.sh
+++ nose.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 rm -r coverage
 set -e
-nosetests -v --with-coverage --cover-inclusive --cover-erase --cover-html --cover-html-dir=./coverage --cover-package=gglsbl3 --with-html-report --html-output-file ./coverage/report.html
+nosetests -v --with-coverage  --cover-erase --cover-html --cover-html-dir=./coverage --cover-package=gglsbl3 --with-html-report --html-output-file ./coverage/report.html
 chromium-browser  --new-window
 chromium-browser ./coverage/index.html
 chromium-browser ./coverage/report.html
